12182acbc3017cf46b26900de1cf880e40c41bb2
Wajid <ansariwajid9999@gmail.com>
2024-12-23 23:20:44 +0530

    testing

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controller/user");
 
-// router is replaced by server
 router
   .get("/", userController.getAllUsers)
   .get("/:id", userController.getUserById)
